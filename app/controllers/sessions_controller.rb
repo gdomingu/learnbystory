99d2bf37b28d9d051c50dc73f5ee27c5a77417dc
jason thein <emailmejason@yahoo.com>
2013-06-19 16:11:08 -0700

    added login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
 
-
   def create
     user = User.find_by_email(params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, :notice => "You are now logged!"
+      redirect_to root_path, :notice => "You are now logged in."
     else
-      flash.now[:alert] = "Oops! Your memory or your fingers suck. Try again."
+      flash.now[:alert] = "Ooops. Typo or Brain Fart. Try again."
       render :new
     end
   end
